6eb14436d46299bb8c846691d809f339de778317
BoRoDaTeNkiY <polpoltrop228@gmail.com>
2024-02-12 23:08:38 +0200

    swagger upload

--- src/photos/photos.controller.ts
+++ src/photos/photos.controller.ts
@@ -3,18 +3,38 @@ import { PhotosService } from './photos.service';
 import { CreatePhotoDto } from './dto/create-photo.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileStorage } from './storage';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PhotoEntity } from './entities/photo.entity';
+import { Repository } from 'typeorm';
 
 @Controller('photos')
 @ApiTags('photos')
 export class PhotosController {
-  constructor(private readonly photosService: PhotosService) {}
+  constructor(
+    @InjectRepository(PhotoEntity)
+    private repository: Repository<PhotoEntity>
+  ) {}
 
   @Post()
-  @UseInterceptors(FileInterceptor('file', {
-    storage: fileStorage,
-  }))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: fileStorage,
+    }),
+  )
+  @ApiConsumes('multipart/from-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        }
+      }
+    }
+  })
   create(@Body() createPhotoDto: CreatePhotoDto) {
-    return this.photosService.create(createPhotoDto);
+    return this.repository.create(createPhotoDto);
   }
 }
