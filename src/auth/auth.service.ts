a51e71cce23e21c09625a52c451aa598b54a3fba
BoRoDaTeNkiY <polpoltrop228@gmail.com>
2024-02-19 18:31:44 +0200

    jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,12 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UserEntity } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-    constructor (private usersService: UsersService) {}
+    constructor (private usersService: UsersService, private jwtService: JwtService) {}
 
     async validateUser(email: string, password: string): Promise<any> {
         const user = await this.usersService.findByEmail(email);
@@ -21,10 +23,18 @@ export class AuthService {
         try {
           const userData = await this.usersService.create(dto);
     
-          return userData
+          return {
+            token: this.jwtService.sign({ id: userData.id }),
+          };
         } catch (err) {
           console.log(err);
-          throw new ForbiddenException('Error during registration');
+          throw new ForbiddenException('Ошибка при регистрации');
         }
+    }
+
+    async login(user: UserEntity) {
+        return {
+          token: this.jwtService.sign({ id: user.id }),
+        };
     }
 }
