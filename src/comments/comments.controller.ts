2e4006c03fbabb59339df911df96b06bd29739ca
BoRoDaTeNkiY <polpoltrop228@gmail.com>
2024-02-20 16:08:32 +0200

    guard

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/Guards/jwt.guard';
 
 @Controller('comments')
+@ApiTags('comments')
+@UseGuards(JwtAuthGuard)
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
